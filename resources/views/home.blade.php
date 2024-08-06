e25cb5b66abdd0cf7724a0c13e183a08fc6378bb
obahchimaobi <anthonyobah37@gmail.com>
2024-08-06 19:00:37 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>VtuBiz - @yield('title', 'Home')</title>
+    <title>EasyPayHub - @yield('title', 'One-Stop Solution for All Your Top-Ups')</title>
 
     {{-- BOOTSTRAP CDN --}}
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
@@ -19,9 +19,9 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-sm navbar-light bg-warning z-3 position-absolute w-100">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">SITENAME</a>
+    <nav class="navbar navbar-expand-sm z-3 position-fixed w-100" style="background: linear-gradient(135deg, #1e3c72 0%, #2a5298 50%, #8e44ad 100%);">
+        <div class="container">
+            <a class="navbar-brand text-light fw-bold" href="{{ route('home') }}" style="background: linear-gradient(135deg, #FF7E5F, #FEB47B); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">EasyPayHub</a>
 
             <button class="navbar-toggler d-lg-none rounded-0 shadow-none" type="button" data-bs-toggle="collapse"
                 data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
@@ -30,78 +30,40 @@
 
             </button>
             <div class="collapse navbar-collapse" id="collapsibleNavId">
-                <ul class="navbar-nav me-auto mt-2 mt-lg-0 m-auto" style="font-size: 14px;">
+                <ul class="navbar-nav ms-auto mt-2 mt-lg-0" style="font-size: 14px;">
                     <li class="nav-item">
-                        <a class="nav-link me-3" href="#" aria-current="page">Home</a>
+                        <a class="nav-link me-3 text-light" href="#" aria-current="page">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-3" href="#">About</a>
+                        <a class="nav-link me-3 text-light" href="#">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link me-3" href="#">Contact</a>
+                        <a class="nav-link me-3 text-light" href="#">Contact</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownId">
-                            <a class="dropdown-item" href="#">Action 1</a>
-                            <a class="dropdown-item" href="#">Action 2</a>
-                        </div>
+                    <li class="nav-item">
+                        <a href="" class="nav-link me-2 me-3 text-light">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link me-2 me-3 text-light">Login</a>
                     </li>
                 </ul>
-                <a href="" class="btn btn-success me-2" style="font-size: 14px;">Register</a>
-                <a href="" class="btn btn-success" style="font-size: 14px;">Login</a>
             </div>
         </div>
     </nav>
 
-    <br><br><br>
-    {{-- SIDEBAR --}}
-    <nav class="navbar">
-        <div class="container-fluid">
+    <br><br><br><br><br>
+    {{-- BANNER --}}
+    <div class="container mt-5">
+        <div class="row d-flex align-items-center">
+            <div class="col-xl-6">
+                <h1 style="font-family: 'Roboto', sans-serif; font-weight: 700;" class=" text-center text-xl-start">Welcome To Sitename</h1>
+            </div>
 
-            <button class="navbar-toggler rounded-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <i class="fa-solid fa-bars"></i>
-            </button>
-            
-            <div class="offcanvas offcanvas-start w-25" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="d-flex mt-3" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
+            <div class="col-xl-6">
+                <img src="{{ asset('assets/images/slider.png') }}" alt="" class="w-100 img-fluid">
             </div>
         </div>
-    </nav>
+    </div>
 
 
     <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
